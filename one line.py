b6e52dbc70ff95e902ee448292b09e7f37cfb26b
Tanxay <125134137+Tanxay@users.noreply.github.com>
2023-05-06 21:55:57 +0530

    Create one line.py

---
+++ one line.py
@@ -0,0 +1,10 @@
+how to print no. in single line  (end = '')
+
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(1,n+1):
+        print(i,end = '')
+        
+        
+        output == 1234...n
