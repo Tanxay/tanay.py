813c88911926b5942e3dd20b054b7ad774471bd9
Tanxay <125134137+Tanxay@users.noreply.github.com>
2023-05-06 21:44:42 +0530

    Create funleap.py

---
+++ funleap.py
@@ -0,0 +1,15 @@
+def is_leap(year):
+    
+    # variable to check the leap year
+    leap = False
+    
+    # One if statement that contains multiple conditions
+    if ((year % 400 == 0) and (year % 100 == 0)) | ((year % 4 ==0) and (year % 100 != 0)):
+        
+       # change leap to True
+        leap = True
+
+    
+    return leap
+year = int(input())
+print(is_leap(year))
